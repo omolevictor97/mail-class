d951567a71d1449df558f88b64c9cf55f50e4027
omolevictor97 <omolevictor97@gmail.com>
2023-12-25 01:55:23 +0100

    constants folder added

---
+++ constants/__init__.py
@@ -0,0 +1,7 @@
+CUSTOM_STOPWORDS = [
+    "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't",
+    'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't",
+    'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan',
+    "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't",
+    'wouldn', "wouldn't"
+]
